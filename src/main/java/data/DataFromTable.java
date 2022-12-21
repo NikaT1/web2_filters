80a19849887351a15e3aaf6f76fc282b94f3e23b
Troynikova Veronika <troynikv@gmail.ru>
2022-12-21 19:32:15 +0300

    Add lombok

--- src/main/java/data/DataFromTable.java
+++ src/main/java/data/DataFromTable.java
@@ -1,5 +1,12 @@
 package data;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class DataFromTable {
     private double x;
     private double y;
@@ -8,70 +15,4 @@ public class DataFromTable {
     private String answer;
     private boolean isValid;
     private double scripTime;
-
-    public DataFromTable(double x, double y, double r, String time, double scriptTime, String answer, boolean isValid) {
-        this.x = x;
-        this.y = y;
-        this.r = r;
-        this.time = time;
-        this.answer = answer;
-        this.isValid = isValid;
-        this.scripTime = scriptTime;
-    }
-
-    public double getScripTime() {
-        return scripTime;
-    }
-
-    public void setScripTime(double scripTime) {
-        this.scripTime = scripTime;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public void setR(double r) {
-        this.r = r;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-    public void setIsValid(boolean isValid) {
-        this.isValid = isValid;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public double getR() {
-        return r;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public boolean getIsValid() {
-        return isValid;
-    }
 }
